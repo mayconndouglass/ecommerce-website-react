fd529395bceed3b09cec57c9a66c7e5c3a5b430c
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-07-20 20:56:01 -0300

    Initial structure

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  white: "fff",
+
+  'primary': '#4E4D93',
+  'dark-primary': '#2A254B',
+  'light-gray': '#F9F9F9',
+  'border-gray': '#EBE8F4',
+  'border-dark': '#CAC6DA'
+} as const
