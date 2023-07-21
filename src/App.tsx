c6b3c6da7b5808aa51300a38f447258ab8dde0f8
talison-cardoso <talisong.me@gmail.com>
2023-07-21 12:00:49 -0300

    style: indented with 4 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <>Hello Word</>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <h1>Hello Word</h1>
+        </ThemeProvider>
+    )
 }
