2b942e2ed1dbe7e089489a16de8474a6b7bc1e6c
talison-cardoso <talisong.me@gmail.com>
2023-07-28 16:55:55 -0300

    chore: add Cart page

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+
+import { Header } from '../../components/Header'
+import { Footer } from '../Home/components/Footer'
+import { Products } from './components/Products'
+
+export const Cart = () => {
+    return (
+        <S.Container>
+            <Header />
+            <main>
+                <Products />
+            </main>
+            <Footer />
+        </S.Container>
+    )
+}
