42613c20ef346c70f4108a6dc3e711d27f0b180e
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-07-29 02:49:00 -0300

    fix: imports

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,7 @@
 import * as S from './styles'
 
 import { Header } from '../../components/Header'
-import { Footer } from '../Home/components/Footer'
+import { Footer } from '../../components/Footer'
 import { Products } from './components/Products'
 
 export const Cart = () => {
