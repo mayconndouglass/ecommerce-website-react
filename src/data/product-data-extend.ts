2ca6b274c5d31255648dad37c98b69085bc1a54d
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-07-29 17:15:17 -0300

    refactor: Product data

--- src/data/product-data-extend.ts
+++ src/data/product-data-extend.ts
@@ -2,63 +2,159 @@ import { ProductDataType } from '../@types/product-data-type'
 
 export const productDataExtend: ProductDataType[] = [
     {
+        id: 1,
         img: './assets/images/chair.png',
+        bigImg: './assets/images/big-chair.jpg',
         title: 'The Dandy chair',
         price: '£250',
+        description: 'A timeless design, with premium materials features as one of our most popular and iconic pieces. The dandy chair is perfect for any stylish living space with beech legs and lambskin leather upholstery.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
+        id: 2,
         img: './assets/images/vases.png',
+        bigImg: './assets/images/big-vases.jpg',
         title: 'Rustic Vase Set',
-        price: '£155'
+        price: '£155',
+        description: 'Add a rustic touch to your decor with this charming vase set. Made with high-quality natural materials, these vases add charm and elegance to your space.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
+        id: 3,
         img: './assets/images/silky-vase.png',
+        bigImg: './assets/images/big-silky-vase.jpg',
         title: 'The Silky Vase',
-        price: '£125'
+        price: '£125',
+        description: 'The Silky vase is an elegant addition to your home. With smooth lines and a silky finish, this vase brings sophistication and beauty to any environment.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
+        id: 4,
         img: './assets/images/lamp.png',
+        bigImg: './assets/images/big-lamp.jpg',
         title: 'The Lucy Lamp',
-        price: '£399'
+        price: '£399',
+        description: 'The Lucy lamp is a masterpiece of modern design. Made with high-quality materials and attention to detail, this lamp provides soft and welcoming lighting for your space.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
-        img: './assets/images/chair.png',
-        title: 'The Dandy chair',
+        id: 5,
+        img: './assets/images/graystone-vase.png',
+        bigImg: './assets/images/big-graystone-vase.jpg',
+        title: 'Graystone Vase',
+        price: '£85',
+        description: 'A timeless ceramic vase with a tri color grey glaze.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
+    },
+    {
+        id: 6,
+        img: './assets/images/basic-white-vase.png',
+        bigImg: './assets/images/big-basic-white-vase.jpg',
+        title: 'Basic white vase',
+        price: '£125',
+        description: 'Beautiful and simple this is one for the classics',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
+    },
+    {
+        id: 7,
+        img: './assets/images/round-brown-wooden-top.png',
+        bigImg: './assets/images/big-round-brown-wooden-top.jpg',
+        title: 'Round Brown Wooden Top',
+        price: '£100',
+        description: ' Enhance your living space with this elegant Round Brown Wooden Top. Crafted with high-quality brown wood, this round tabletop adds a touch of natural warmth to any room. Its versatile design complements various decor styles, making it a perfect addition to your home.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
+    },
+    {
+        id: 8,
+        img: './assets/images/wood-chair.png',
+        bigImg: './assets/images/big-wood-chair.jpg',
+        title: 'Wood Chair',
         price: '£250',
+        description: 'Embrace the beauty of simplicity with our Wood Chair. Designed with clean lines and sturdy construction, this chair combines style and comfort effortlessly. The natural wood finish complements any interior, making it a classic choice for your dining or living area.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
-        img: './assets/images/vases.png',
-        title: 'Rustic Vase Set',
-        price: '£155'
+        id: 9,
+        img: './assets/images/chandelier-lamp.png',
+        bigImg: './assets/images/big-chandelier-lamp.jpg',
+        title: 'Pendant Chandelier Lamp',
+        price: '£400',
+        description: 'Elevate your space with the captivating Pendant Chandelier Lamp. Its graceful design and dazzling crystals create a mesmerizing focal point in any room. The adjustable height allows for customized illumination, making it a luxurious addition to your home decor.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
-        img: './assets/images/silky-vase.png',
-        title: 'The Silky Vase',
-        price: '£125'
+        id: 10,
+        img: './assets/images/porcelain-vase.png',
+        bigImg: './assets/images/big-porcelain-vase.jpg',
+        title: 'Porcelain Vase',
+        price: '£95',
+        description: 'Add an exquisite touch to your decor with our Porcelain Vase. Handcrafted with intricate details, this vase exudes timeless elegance. Its delicate design showcases the beauty of porcelain, making it a stunning accent piece for your living room or bedroom.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
-        img: './assets/images/lamp.png',
-        title: 'The Lucy Lamp',
-        price: '£399'
+        id: 11,
+        img: './assets/images/blue-armchair.jpg',
+        bigImg: './assets/images/blue-armchair.jpg',
+        title: 'Blue Armchair',
+        price: '£457',
+        description: 'Experience ultimate comfort and style with our Blue Armchair. Upholstered in soft fabric and featuring a charming blue hue, this armchair complements various interior themes. Its ergonomic design ensures hours of relaxation and enjoyment.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
     {
-        img: './assets/images/chair.png',
-        title: 'The Dandy chair',
-        price: '£250',
-    },
-    {
-        img: './assets/images/vases.png',
-        title: 'Rustic Vase Set',
-        price: '£155'
-    },
-    {
-        img: './assets/images/silky-vase.png',
-        title: 'The Silky Vase',
-        price: '£125'
-    },
-    {
-        img: './assets/images/lamp.png',
-        title: 'The Lucy Lamp',
-        price: '£399'
+        id: 12,
+        img: './assets/images/colorful-chairs.png',
+        bigImg: './assets/images/big-colorful-chairs.jpg',
+        title: 'Colorful Chairs',
+        price: '£72',
+        description: 'Infuse vibrant energy into your living space with our Colorful Chairs. With a range of captivating colors, these chairs become a delightful focal point in any room. Made with durable materials and a sleek design, they are a perfect combination of style and functionality.',
+        dimensions: {
+            height: '110cm',
+            width: '75cm',
+            depth: '50cm'
+        }
     },
 ]
