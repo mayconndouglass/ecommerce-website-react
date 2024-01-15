369d2027de45ec896a573d0bbaa4b7c9b394b26f
talison-cardoso <talisong.me@gmail.com>
2024-01-15 12:01:35 -0300

    fix: default behavior when rendering the page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,8 +6,13 @@ import { Features } from '../../components/Features'
 import { OurService } from './components/OurService'
 import { FooterV2 } from '../../components/FooterV2'
 import { SignupV2 } from '../../components/SignupV2'
+import { useEffect } from 'react'
 
 export const About = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <>
             <Banner />
